fd751ac93c7fce60355a97a31be4165538d952a8
garsan84 <jgrsan84@gmail.com>
2021-12-17 20:34:18 +0100

    structure completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import WalletCard from './WalletCard';
+import WalletCardEthers from'./WalletCardEthers';
+
+function App() {
+  return (
+    <div className="App">
+        <WalletCard/>
+       <WalletCardEthers/>
+    </div>
+  );
+}
+
+export default App;
